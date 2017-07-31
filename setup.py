3448a4273822e4e03a97d0a3f20bec4dfdd0cc3f
Santiago Osorio <santiagoosorio1@gmail.com>
2017-07-31 15:41:59 -0500

    Updated package version

--- setup.py
+++ setup.py
@@ -3,15 +3,14 @@ from setuptools import setup
 setup(
   name='prexview',
 
-  version='1.0.1',
-
-  description='A pip library to use PrexView a fast, scalable and very friendly service for programatic HTML, PDF, PNG or JPG generation using JSON or XML data.',
+  version='1.1.0',
+  description='A Python package to use PrexView a fast, scalable and friendly service for programatic HTML, PDF, PNG or JPG generation using JSON or XML data.',
 
   # long_description=long_description,
 
   url='https://github.com/prexview/prexview-python',
 
-  download_url='https://github.com/prexview/prexview-python/archive/1.0.1.tar.gz',
+  download_url='https://github.com/prexview/prexview-python/archive/1.1.0.tar.gz',
 
   # Author details
   author='PrexView',
@@ -24,7 +23,7 @@ setup(
     #   3 - Alpha
     #   4 - Beta
     #   5 - Production/Stable
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
 
     # Indicate who your project is intended for
     'Intended Audience :: Developers',
@@ -43,42 +42,38 @@ setup(
     'Programming Language :: Python :: 3.5',
   ],
 
-  keywords=['xml', 'json', 'xml-to-pdf', 'json-to-pdf'],
+  keywords=[
+    'xml',
+    'json',
+    'converter',
+    'convert',
+    'transform',
+    'image',
+    'xml-parser',
+    'json-parser',
+    'document-conversion',
+    'xml-to-pdf',
+    'xml-to-png',
+    'xml-to-jpg',
+    'xml-to-html',
+    'json-to-pdf',
+    'json-to-png',
+    'json-to-jpg',
+    'json-to-html',
+    'html-to-pdf',
+    'html-to-png',
+    'html-to-jpg',
+    'pdf',
+    'html',
+    'png',
+    'jpg',
+    'renderer',
+    'prexview'
+  ],
 
   packages=[],
 
-  py_modules=["prexview"],
+  py_modules=['prexview'],
 
-  install_requires=['requests'],
-
-  # List additional groups of dependencies here (e.g. development
-  # dependencies). You can install these using the following syntax,
-  # for example:
-  # $ pip install -e .[dev,test]
-  # extras_require={
-  #   'dev': ['check-manifest'],
-  #   'test': ['coverage'],
-  # },
-
-  # If there are data files included in your packages that need to be
-  # installed, specify them here.  If using Python 2.6 or less, then these
-  # have to be included in MANIFEST.in as well.
-  # package_data={
-  #   'sample': ['package_data.dat'],
-  # },
-
-  # Although 'package_data' is the preferred approach, in some case you may
-  # need to place data files outside of your packages. See:
-  # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-  # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-  # data_files=[('my_data', ['data/data_file'])],
-
-  # To provide executable scripts, use entry points in preference to the
-  # "scripts" keyword. Entry points provide cross-platform support and allow
-  # pip to create the appropriate form of executable for the target platform.
-  # entry_points={
-  #   'console_scripts': [
-  #     'prexview=prexview:main',
-  #   ],
-  # },
+  install_requires=['requests']
 )
